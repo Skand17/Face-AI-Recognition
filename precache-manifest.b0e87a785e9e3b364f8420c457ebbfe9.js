1063aa916f64390c6f93688483c9978855322f11
skand mishra <skand.mishra@mobicules.com>
2020-05-02 01:10:37 +0530

    Updates

---
+++ precache-manifest.b0e87a785e9e3b364f8420c457ebbfe9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "118e573465360a068c0d3db1d4e908d0",
+    "url": "/Face-AI-Recognition/index.html"
+  },
+  {
+    "revision": "f812ac719053a4720ec2",
+    "url": "/Face-AI-Recognition/static/css/main.cdd64140.chunk.css"
+  },
+  {
+    "revision": "620bc283d2b7a4729538",
+    "url": "/Face-AI-Recognition/static/js/2.429b0ad8.chunk.js"
+  },
+  {
+    "revision": "95b3c6d02726ade2fa567664bec5f4ff",
+    "url": "/Face-AI-Recognition/static/js/2.429b0ad8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f812ac719053a4720ec2",
+    "url": "/Face-AI-Recognition/static/js/main.41b69faf.chunk.js"
+  },
+  {
+    "revision": "3dc091d10279e6d0f221",
+    "url": "/Face-AI-Recognition/static/js/runtime-main.ecabc676.js"
+  }
+]);
